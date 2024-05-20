9de80081b8fe3ebfc57157e2fb5a4b7debb169e8
ishayankoo001 <ishayankoo@icloud.com>
2024-05-20 17:34:07 +0200

    Q31 Done.

--- 31/program.py
+++ 31/program.py
@@ -17,12 +17,38 @@ def es31(fname1,fname2):
     letters are NIBPGAD.
 
     '''
+    words = []
+    changes = []
+    with open(fname1) as f:
+        for line in f:
+            words.extend(line.strip().split())
+        chars = [chr(i) for i in range(97,97+26)]
+        sums = 0
+        newwords = []
+        newwords[:] = words
+        for c in chars:
+            newlis = sum([c in words[i] for i in range(len(words))])
+            if newlis % 2 != 0:
+                changes.append(c)
+    with open(fname1) as f:
+        with open(fname2,"w")as f2:
+            for line in f:
+                print(line)
+                for char in changes:
+                    line = line.replace(char, char.capitalize())
+                f2.write(line)
+    return len(changes)
+
     # insert here your code
 
 
 
 
 
+
+es31("ftesto4.txt","test.txt")
+with open("check.txt","w") as check:
+    check.write('''SalvE, Ho Una doManda:\nMa lo zERo E' Un nUMERo paRi o diSpaRi?\nSo cHE pUo' SEMbRaRE Una banaliTa', a naSo diREi cHE lo zERo E' Un nUMERo paRi\nMa non SapREi coME GiUSTificaRE la RiSpoSTa.\nConfido nEl voSTRo aiUTo!''')
 
 
 
